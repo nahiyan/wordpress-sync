2f37e90e7120c77a8ed557a2109b7fb4586c6638
nahiyan.alamgir <nahiyan.alamgir@vivasoftltd.com>
2022-05-22 12:58:30 +0600

    Integrate the GitHub Repo options fetcher in the Config class

--- config.php
+++ config.php
@@ -3,4 +3,28 @@
 class Config
 {
     public static $userId = 1;
+
+    public static function getGitHubRepoUrl()
+    {
+        $options = Config::options();
+        if ($options != null) {
+            return $options["github_repository_url_0"];
+        }
+
+        return "";
+    }
+    public static function getGitHubRepoBranch()
+    {
+        $options = Config::options();
+        if ($options != null) {
+            return $options["github_repository_branch_1"];
+        }
+
+        return "";
+    }
+
+    private static function options()
+    {
+        return get_option(WP_SYNC_OPTIONS_NAME);
+    }
 }
