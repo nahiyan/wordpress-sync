38abe1d8c4d2b2fd796e2575ab2d670f1f99f30e
Nahiyan Alamgir <nahiyan.alamgir0@gmail.com>
2022-05-24 23:16:35 +0600

    Switch to Monolog for logging

--- src/Logger.php
+++ src/Logger.php
@@ -4,15 +4,31 @@ namespace Vivasoft\WpSync;
 
 class Logger
 {
+    private static $logger = null;
+    public static function get()
+    {
+        if (Logger::$logger == null) {
+            Logger::$logger = new \Monolog\Logger('debug');
+            $handler = new \Monolog\Handler\StreamHandler(path_join(Config::getBaseDir(), "logs/debug.log"), \Monolog\Logger::INFO);
+            $handler->setFormatter(new \Monolog\Formatter\LineFormatter(
+                null, // Format of message in log, default [%datetime%] %channel%.%level_name%: %message% %context% %extra%\n
+                null, // Datetime format
+                true, // allowInlineLineBreaks option, default false
+                true// discard empty Square brackets in the end, default false
+            ));
+            Logger::$logger->pushHandler($handler);
+        }
+
+        return Logger::$logger;
+    }
     public static function debug($key, $message)
     {
-        $logFilePath = path_join(Config::getBaseDir(), 'logs' . DIRECTORY_SEPARATOR . 'debug.log');
-        error_log(($key != null ? $key . ": " : "") . $message . PHP_EOL, 3, $logFilePath);
+        Logger::get()->info(($key != null ? $key . ": " : "") . $message);
+        // $logFilePath = path_join(Config::getBaseDir(), 'logs' . DIRECTORY_SEPARATOR . 'debug.log');
+        // error_log(($key != null ? $key . ": " : "") . $message . PHP_EOL, 3, $logFilePath);
     }
     public static function debugJson($key, $json)
     {
-        $logFilePath = path_join(Config::getBaseDir(), 'logs' . DIRECTORY_SEPARATOR . 'debug.log');
-        $message = json_encode($json, JSON_PRETTY_PRINT);
-        error_log(($key != null ? $key . ": " : "") . $message . PHP_EOL, 3, $logFilePath);
+        Logger::get()->info($key . ": " . json_encode($json, JSON_PRETTY_PRINT));
     }
 }
